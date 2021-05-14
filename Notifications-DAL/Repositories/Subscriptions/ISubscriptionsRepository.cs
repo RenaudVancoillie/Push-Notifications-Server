0de035417a91aec33c4a00f5e9d600fa9dcec010
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-14 13:20:24 +0200

    Add SubscriptionsRepository

---
+++ Notifications-DAL/Repositories/Subscriptions/ISubscriptionsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Notifications_DAL.DTO.Subscriptions;
+
+namespace Notifications_DAL.Repositories.Subscriptions
+{
+    public interface ISubscriptionsRepository
+    {
+        IEnumerable<SubscriptionDTO> GetAll();
+    }
+}
---
+++ Notifications-DAL/Repositories/Subscriptions/SubscriptionsRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Notifications_DAL.Database;
+using Notifications_DAL.DTO.Subscriptions;
+
+namespace Notifications_DAL.Repositories.Subscriptions
+{
+    public class SubscriptionsRepository : ISubscriptionsRepository
+    {
+        private readonly NotificationsContext db;
+        private readonly IMapper mapper;
+
+        public SubscriptionsRepository(NotificationsContext notificationsContext,
+                                       IMapper mapper)
+        {
+            db = notificationsContext;
+            this.mapper = mapper;
+        }
+
+        public IEnumerable<SubscriptionDTO> GetAll()
+        {
+            return db.Subscriptions
+                .ProjectTo<SubscriptionDTO>(mapper.ConfigurationProvider)
+                .AsEnumerable();
+        }
+    }
+}
