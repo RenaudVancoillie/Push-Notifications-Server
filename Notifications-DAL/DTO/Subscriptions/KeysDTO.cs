f216bf92ccd322853b207c8aed725111c1c09cfb
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-14 13:09:55 +0200

    Add SubscriptionDTO with KeysDTO

---
+++ Notifications-DAL/DTO/Subscriptions/KeysDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifications_DAL.DTO.Subscriptions
+{
+    public class KeysDTO
+    {
+        public string P256hd { get; set; }
+        public string Auth { get; set; }
+    }
+}
---
+++ Notifications-DAL/DTO/Subscriptions/SubscriptionDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifications_DAL.DTO.Subscriptions
+{
+    public class SubscriptionDTO
+    {
+        public string Endpoint { get; set; }
+        public float ExpirationDate { get; set; }
+        public KeysDTO Keys { get; set; }
+    }
+}
