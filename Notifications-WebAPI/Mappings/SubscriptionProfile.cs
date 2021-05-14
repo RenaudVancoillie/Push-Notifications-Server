dffd1c8c179c04652455d503858cc528612d1785
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-14 13:14:59 +0200

    Use AutoMapper

---
+++ Notifications-WebAPI/Mappings/SubscriptionProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Notifications_DAL.DTO.Subscriptions;
+using Notifications_DAL.Models;
+
+namespace Notifications_WebAPI.Mappings
+{
+    public class SubscriptionProfile : Profile
+    {
+        public SubscriptionProfile()
+        {
+            CreateMap<Subscription, SubscriptionDTO>();
+            CreateMap<Keys, KeysDTO>();
+        }
+    }
+}
